b94c7737a10a727ac47f74b8384124018d1732fc
Kang <555836+cnkang@users.noreply.github.com>
2026-01-27 15:07:57 +0800

    feat(web): update common UI components
    - Update HoverButton component implementation
    - Update LoadingSpinner component
    - Update NaiveProvider wrapper
    - Update SvgIcon and UserAvatar components
    - Update common components index

--- apps/web/src/components/common/HoverButton/index.vue
+++ apps/web/src/components/common/HoverButton/index.vue
@@ -5,8 +5,8 @@ import { computed } from 'vue'
 import Button from './Button.vue'
 
 interface Props {
-	tooltip?: string;
-	placement?: PopoverPlacement;
+  tooltip?: string
+  placement?: PopoverPlacement
 }
 
 // Use reactive props destructuring (Vue 3.5+ feature)
@@ -14,30 +14,30 @@ const { tooltip = '', placement = 'bottom' } = defineProps<Props>()
 
 // Use defineEmits with modern syntax
 const emit = defineEmits<{
-	click: [];
+  click: []
 }>()
 
 const showTooltip = computed(() => Boolean(tooltip))
 
 function handleClick() {
-	emit('click')
+  emit('click')
 }
 </script>
 
 <template>
-	<div v-if="showTooltip">
-		<NTooltip :placement="placement" trigger="hover">
-			<template #trigger>
-				<Button @click="handleClick">
-					<slot />
-				</Button>
-			</template>
-			{{ tooltip }}
-		</NTooltip>
-	</div>
-	<div v-else>
-		<Button @click="handleClick">
-			<slot />
-		</Button>
-	</div>
+  <div v-if="showTooltip">
+    <NTooltip :placement="placement" trigger="hover">
+      <template #trigger>
+        <Button @click="handleClick">
+          <slot />
+        </Button>
+      </template>
+      {{ tooltip }}
+    </NTooltip>
+  </div>
+  <div v-else>
+    <Button @click="handleClick">
+      <slot />
+    </Button>
+  </div>
 </template>
