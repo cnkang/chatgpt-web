8b9e3d1447d3937ba7fa8dd0e066864d96a59065
Kang <555836+cnkang@users.noreply.github.com>
2026-01-27 15:40:28 +0800

    style(chat): format chat views hooks and layout

--- apps/web/src/views/chat/layout/Layout.vue
+++ apps/web/src/views/chat/layout/Layout.vue
@@ -19,42 +19,37 @@ const { isMobile } = useBasicLayout()
 
 const collapsed = computed(() => appStore.siderCollapsed)
 
-const _needPermission = computed(
-	() => !!authStore.session?.auth && !authStore.token,
-)
+const _needPermission = computed(() => !!authStore.session?.auth && !authStore.token)
 
 const getMobileClass = computed(() => {
-	if (isMobile.value) return ['rounded-none', 'shadow-none']
-	return ['border', 'rounded-md', 'shadow-md', 'dark:border-neutral-800']
+  if (isMobile.value) return ['rounded-none', 'shadow-none']
+  return ['border', 'rounded-md', 'shadow-md', 'dark:border-neutral-800']
 })
 
 const getContainerClass = computed(() => {
-	return ['h-full', { 'pl-[260px]': !isMobile.value && !collapsed.value }]
+  return ['h-full', { 'pl-[260px]': !isMobile.value && !collapsed.value }]
 })
 </script>
 
 <template>
-	<div
-		class="h-full dark:bg-[#24272e] transition-all"
-		:class="[isMobile ? 'p-0' : 'p-4']"
-	>
-		<div class="h-full overflow-hidden" :class="getMobileClass">
-			<NLayout class="z-40 transition" :class="getContainerClass" has-sider>
-				<Sider />
-				<NLayoutContent class="h-full">
-					<Suspense>
-						<template #default>
-							<RouterView v-slot="{ Component, route }">
-								<component :is="Component" :key="route.fullPath" />
-							</RouterView>
-						</template>
-						<template #fallback>
-							<LoadingSpinner text="Loading chat..." />
-						</template>
-					</Suspense>
-				</NLayoutContent>
-			</NLayout>
-		</div>
-		<Permission :visible="_needPermission" />
-	</div>
+  <div class="h-full dark:bg-[#24272e] transition-all" :class="[isMobile ? 'p-0' : 'p-4']">
+    <div class="h-full overflow-hidden" :class="getMobileClass">
+      <NLayout class="z-40 transition" :class="getContainerClass" has-sider>
+        <Sider />
+        <NLayoutContent class="h-full">
+          <Suspense>
+            <template #default>
+              <RouterView v-slot="{ Component, route }">
+                <component :is="Component" :key="route.fullPath" />
+              </RouterView>
+            </template>
+            <template #fallback>
+              <LoadingSpinner text="Loading chat..." />
+            </template>
+          </Suspense>
+        </NLayoutContent>
+      </NLayout>
+    </div>
+    <Permission :visible="_needPermission" />
+  </div>
 </template>
